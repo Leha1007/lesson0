4cfc4dd84975280f01481049f70c1e17514fe8ed
Leha1007 <alukasenko08@gmail.com>
2024-07-21 09:56:15 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+#1st program
+print((9**0.5)*5)
+
+#2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3rd program
+print((1234//10) % 100 + (5678//10) % 100)
+
+#4th program
+a = int(13.42 * 100)
+b = int(42.13 * 100)
+print(a // 100 == b % 100 or b // 100 == a % 100)
+
